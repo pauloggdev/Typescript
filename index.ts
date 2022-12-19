12f23321d4dd55356426d8597b708e7fde3603e5
pauloggdev <pauloggjoao@gmail.com>
2022-12-19 16:48:32 +0100

    add create product

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log('teste');
+class Produto{
+
+    private nome:string;
+    private preco: number;
+
+    constructor(nome:string, preco:number){
+        this.nome = nome;
+        this.preco = preco;
+    }
+    mostrar(){
+        console.log(`PRODUTO: ${this.nome} PRECO: ${this.preco}`);
+    }
+}
+
+const produto = new Produto('Produto 01', 200);
+produto.mostrar();
