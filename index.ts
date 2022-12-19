e256e89ab40c737f18c8e7f2453dea297c08cfa8
pauloggdev <pauloggjoao@gmail.com>
2022-12-19 16:59:38 +0100

    =add description teste branch

--- index.ts
+++ index.ts
@@ -3,14 +3,21 @@ class Produto{
     private nome:string;
     private preco: number;
 
+
     constructor(nome:string, preco:number){
         this.nome = nome;
         this.preco = preco;
     }
     mostrar(){
         console.log(`PRODUTO: ${this.nome} PRECO: ${this.preco}`);
+        console.log(`Console adicionado para testar outra branch`)
     }
 }
+
+
+
+
+
 
 const produto = new Produto('Produto 01', 200);
 produto.mostrar();
